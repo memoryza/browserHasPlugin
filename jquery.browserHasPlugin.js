c4e95d6ef6de2cd6ddc211b2e06a6ade5813c96c
memoryza <jincai.wang@foxmail.com>
2014-03-07 10:39:03 +0800

    --amend

--- jquery.browserHasPlugin.js
+++ jquery.browserHasPlugin.js
@@ -2,10 +2,18 @@
     var plugins = navigator.plugins;
     $.browserHasPlugin = function(name) {
         name = name.toLowerCase();
-        if(plugins && plugins.length) {
-            for(var i = plugins.length; i >= 0; --i) {
-                if(plugins[i] && plugins[i].name && plugins[i].name.toLowerCase().indexOf(name) > -1) {
-                    return true;
+        if($.browser.msie) {
+            try {
+                new ActiveXObject(name);
+                return true;
+            } catch(ex) {
+            }
+        } else {
+            if(plugins && plugins.length) {
+                for(var i = plugins.length; i >= 0; --i) {
+                    if(plugins[i] && plugins[i].name && plugins[i].name.toLowerCase().indexOf(name) > -1) {
+                        return true;
+                    }
                 }
             }
         }
